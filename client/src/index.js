ef8df9c28d85edf882f0c72258aba042bc2cc944
KHUSHI 444 <khankhushi4452@gmail.com>
2024-07-27 17:01:12 +0530

    added the basic setup

---
+++ client/src/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import Home from "./views/Home/Home"
+import ShowProduct from './views/ShowProduct/ShowProduct';
+
+import AddProduct from "./views/Addproduct/Addproduct"
+import UpdateProduct from './components/UpdateProduct/UpdateProduct';
+import Signup from "./views/Signup/Signup";
+import Login from "./views/Login/Login";
+import SaleProduct from './views/SaleProduct/SaleProduct';
+const router = createBrowserRouter([{
+  path: '/',
+  element:<Home/>
+},
+
+{
+  path: '/addproduct',
+  element :<AddProduct/> 
+},
+{
+  path: '/showproduct',
+  element :<ShowProduct/> 
+},
+{
+  path: '/updateproduct/:id',
+  element :<UpdateProduct/>
+},
+{
+  path: '/saleproduct/product/:id',
+  element :<SaleProduct/>
+},
+{
+  path: '/signup',
+  element: <Signup />
+},
+{
+  path: '/login',
+  element: <Login/>
+}
+
+]);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <RouterProvider router={router} />
+);
