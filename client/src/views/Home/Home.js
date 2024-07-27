96f350d3c53a7da1d0791f420ceabdf7a4079eed
KHUSHI 444 <khankhushi4452@gmail.com>
2024-07-27 17:24:19 +0530

    design button

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from "../../components/Navbar/Navbar"
+import {Link} from "react-router-dom"
 
 
 function Home(){
@@ -27,6 +28,11 @@ function Home(){
    <p>
    Our mission is to offer a curated selection of premium beauty products for all your skincare..  
    </p>
+   <Link to='/addproduct' className="no-underline">
+   <button className="d-block mt-5 bg-pink-500 py-2 px-10 text-white rounded-lg">
+   Buy Products
+   </button>
+   </Link>
   </div>
  
     </div>  
