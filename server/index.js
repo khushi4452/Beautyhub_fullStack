f8feb516c537c99ea183eef4a2e8b21f83d9da0d
KHUSHI 444 <khankhushi4452@gmail.com>
2024-07-29 20:35:40 +0530

    basic server setup

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from "dotenv"
+import path from 'path';
+
+
+
+dotenv.config();
+
+
+const app = express()
+app.use(express.json());
+
+const __dirname = path.resolve();
+
+const connectionDB = () => {
+
+  const conn = mongoose.connect(process.env.MONGO_URI);
+
+  if (conn) {
+    console.log("MongoDB connected Successfully.")
+  }
+}
+
+connectionDB();
+
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+  });
+}
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`)
+});
