31ef2298d8c55f5e656b4d3824ad2330677dde8e
KHUSHI 444 <khankhushi4452@gmail.com>
2024-07-29 21:35:58 +0530

    Backend is Ready

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ import path from 'path';
 
 
 dotenv.config();
+
 import { getApiHealths } from './controllers/health.js'
+import { postApiSignups , postApiLogins } from './controllers/user.js';
+import { postApiProduct, getApiProductsById, putApiTransactionsById, getApiProducts, getApiAllProducts } from './controllers/product.js';
 
 
 const app = express()
@@ -16,26 +19,45 @@ const __dirname = path.resolve();
 
 const connectionDB = () => {
 
-  const conn = mongoose.connect(process.env.MONGO_URI);
+const conn = mongoose.connect(process.env.MONGO_URI);
 
-  if (conn) {
-    console.log("MongoDB connected Successfully.")
+if (conn) {
+console.log("MongoDB connected Successfully.")
   }
 }
 
 connectionDB();
 
+
 app.get('/api/v1/healths', getApiHealths);
+
+app.post('/api/v1/signups', postApiSignups);
+
+app.post('/api/v1/logins', postApiLogins);
+
+app.post('/api/v1/products', postApiProduct);
+
+app.get('/api/v1/products/user/:id', getApiProductsById);  
+
+
+app.put("/api/v1/products/:id", putApiTransactionsById);
+
+app.get('/api/v1/products/:id', getApiProducts);
+
+
+app.get('/api/v1/products', getApiAllProducts);
+
+
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+ app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+ app.get('*', (req, res) => {
+ res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
   });
 }
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`)
+ console.log(`Server running on port ${process.env.PORT}`)
 });
